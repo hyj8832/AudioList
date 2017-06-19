0ea9cd0f5fe62167cd62ab046039d0d9233dac45
hyj8832 <hyj8832@naver.com>
2017-06-19 20:59:35 +0900

    #4 Button ListView Event Programmming

--- app/src/main/java/com/example/admin/audiolist/MainActivity.java
+++ app/src/main/java/com/example/admin/audiolist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.admin.audiolist;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
       TextView textMusic;
       ProgressBar progress;
       String [] musics={"music","oldie","birdbrainz"};
+      int[] musicResIds={R.raw.music,R.raw.oldie,R.raw.birdbrainz};
+      int selectedMusicID;
+      MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +35,34 @@ public class MainActivity extends AppCompatActivity {
         list.setAdapter(adapter);
         list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);//실제로 선택이 한 개만 되도록
         list.setItemChecked(0,true);//선택한 인덱스 번호가 실행이 ...어쩌고
+        selectedMusicID=musicResIds[0]; //첫번째 노래 실행(기본적으로 )
+        mediaPlayer=MediaPlayer.create(this,selectedMusicID);
 
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {//항목이 클릭되었을 때 실행되는 OnItemClickListener
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) { //선택된 항목의 인덱스 번호 i
+                selectedMusicID=musicResIds[i];
+            }
+        });
+
+        butPlay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   mediaPlayer.start();
+                }
+            });
+        butStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.create(MainActivity.this,selectedMusicID);
+                mediaPlayer.stop();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() { //액티비티가 다른 화면에 가려졌을 때
+        super.onStop();
+        mediaPlayer.stop();
     }
 }
