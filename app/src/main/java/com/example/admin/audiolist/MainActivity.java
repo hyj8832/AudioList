cb8e2d98a8d8c06f22054a073486265e5625eb38
hyj8832 <hyj8832@naver.com>
2017-06-19 20:34:25 +0900

    #1 Set ListView Items

--- app/src/main/java/com/example/admin/audiolist/MainActivity.java
+++ app/src/main/java/com/example/admin/audiolist/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.admin.audiolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+      ListView list;
+      Button butPlay,butStop;
+      TextView textMusic;
+      ProgressBar progress;
+      String [] musics={"music","oldie","birdbrainz"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list=(ListView)findViewById(R.id.list_music);
+        butPlay=(Button)findViewById(R.id.but_play);
+        butStop=(Button)findViewById(R.id.but_stop);
+        textMusic=(TextView)findViewById(R.id.text_music);
+        progress=(ProgressBar)findViewById(R.id.progress_music);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice,musics); //한개만 선택할 수 있는 (노래를)
+        list.setAdapter(adapter);
+        list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);//실제로 선택이 한 개만 되도록
+        list.setItemChecked(0,true);//선택한 인덱스 번호가 실행이 ...어쩌고
+
     }
 }
