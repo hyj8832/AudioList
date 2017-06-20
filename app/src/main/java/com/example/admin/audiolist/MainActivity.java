cdf60eea2ea6cad09d73f05adf3e5e39c2e858aa
hyj8832 <hyj8832@naver.com>
2017-06-20 19:56:02 +0900

    Event Programming Pause Button

--- app/src/main/java/com/example/admin/audiolist/MainActivity.java
+++ app/src/main/java/com/example/admin/audiolist/MainActivity.java
@@ -15,13 +15,14 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
       ListView list;
-      Button butPlay,butStop;
+      Button butPlay,butStop,butPause;
       TextView textMusic;
       ProgressBar progress;
-      String [] musics={"music","oldie","birdbrainz"};
+      String [] musics={"music","oldie","never"};
       int[] musicResIds={R.raw.music,R.raw.oldie,R.raw.birdbrainz};
       int selectedMusicID;
       MediaPlayer mediaPlayer;
+    boolean selectedPauseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         list=(ListView)findViewById(R.id.list_music);
         butPlay=(Button)findViewById(R.id.but_play);
         butStop=(Button)findViewById(R.id.but_stop);
+        butPause=(Button)findViewById(R.id.but_pause);
         textMusic=(TextView)findViewById(R.id.text_music);
         progress=(ProgressBar)findViewById(R.id.progress_music);
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice,musics); //한개만 선택할 수 있는 (노래를)
@@ -41,21 +43,42 @@ public class MainActivity extends AppCompatActivity {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {//항목이 클릭되었을 때 실행되는 OnItemClickListener
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) { //선택된 항목의 인덱스 번호 i
+                mediaPlayer.stop();
                 selectedMusicID=musicResIds[i];
+                mediaPlayer=MediaPlayer.create(MainActivity.this,selectedMusicID); //이 거 때문에 이젠 일지정지 후 다른 곡 실행 가능!
+                progress.setVisibility(View.INVISIBLE);
             }
         });
 
         butPlay.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                   mediaPlayer.start();
+                    if(selectedPauseButton) {
+
+                        mediaPlayer.start();
+                        selectedPauseButton=false;//원래대로 초기화
+                    }
+                    else {
+                        mediaPlayer = MediaPlayer.create(MainActivity.this, selectedMusicID);
+                        mediaPlayer.start();
+                    }
+                    progress.setVisibility(View.VISIBLE);
                 }
             });
         butStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mediaPlayer.create(MainActivity.this,selectedMusicID);
                 mediaPlayer.stop();
+                progress.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        butPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedPauseButton=true;
+                mediaPlayer.pause();
+                progress.setVisibility(View.INVISIBLE);
             }
         });
     }
